139b707bb75df09cc07258b7d1431d5119bb7d3c
Paul Merrill <pdm@pdm.me>
2020-04-02 16:55:58 -0700

    backend: Add CORS header for localhost:3000

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -4,14 +4,30 @@ import express from 'express';
 import setupProfile from './profile.js';
 import setupQuestion from './question.js';
 
+const allowedOrigins = {
+    'http://localhost:3000': true,
+};
+
 function log(req, res, next) {
     const { method, path } = req;
     console.log(`${method} ${path}`);
     next();
 }
 
+function cors(req, res, next) {
+    const origin = req.headers.origin;
+
+    if (origin in allowedOrigins) {
+        res.set('Access-Control-Allow-Origin', origin);
+        res.vary('Origin');
+    }
+
+    next();
+}
+
 function setupRoutes(router) {
     router.use(log);
+    router.use(cors);
 
     router.use(bodyParser.json());
     router.use(bodyParser.urlencoded({ extended: false }));
